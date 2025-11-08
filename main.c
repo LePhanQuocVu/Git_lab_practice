2fc1d8bdea6f1efb06ca76d34aa8c16247917760
QuocVu26 <vu.lephandtr2k2.hcmut.edu.vn>
2025-11-08 18:42:40 +0700

    team ban vien code

--- main.c
+++ main.c
@@ -4,4 +4,5 @@ int main() {
     return 0;
     // team member code ở đây và gây ra conflict
     // leader gcode ơ đây
+    // team tieeps tuc code o day va gay ra confflict voi leader chua commit
 }
