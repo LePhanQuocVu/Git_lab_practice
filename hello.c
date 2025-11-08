c78122660c6111ba6244c82a0a8853a33a3a3e92
vule <vu.le.xh@bp.renesas.com>
2025-11-08 14:34:22 +0800

    hcmut add code

--- hello.c
+++ hello.c
@@ -3,4 +3,5 @@ void print_hello(){
     printf("Hello minh ne lan 2");
     printf("Su dung de test git rebase");
     printf("Su dung de dung: git pull --rebase");
+    printf("Day la code từ email: hcmut");
 }
