07de2fb585cb41a44d65872692ef144c8328743a
vule <vu.le.xh@bp.renesas.com>
2025-11-08 19:58:37 +0800

    leader cho conflict ở main

--- main.c
+++ main.c
@@ -8,4 +8,5 @@ int main() {
     // team tieeps tuc code o day va gay ra confflict voi leader chua commit
     //leader code ở đây tiếp nhưng chưa commit
     // team vaanx tiep tục code ở đây biết là leader đã thêm dòng vào số 6
+    // leader cho conflice ở main
 }
