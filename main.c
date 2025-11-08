26b4f0a9273b6f47c38dedf9189bddaec333d8ff
vule <vu.le.xh@bp.renesas.com>
2025-11-08 19:47:13 +0800

    leader code và gây ra conflict cho member khi pull về

--- main.c
+++ main.c
@@ -3,6 +3,8 @@
 int main() {
     return 0;
     // team member code ở đây và gây ra conflict
+    // leader code ở đây để gây ra conflict cho team member khi pull về
     // leader gcode ơ đây
     // team tieeps tuc code o day va gay ra confflict voi leader chua commit
+    //leader code ở đây tiếp nhưng chưa commit
 }
