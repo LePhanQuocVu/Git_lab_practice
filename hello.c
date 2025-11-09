70ec473dc5f88eba5c0cb7bcd5a0c1d70f2193fd
QuocVu26 <vu.lephandtr2k2.hcmut.edu.vn>
2025-11-09 08:33:21 +0700

    member cod

--- hello.c
+++ hello.c
@@ -29,4 +29,6 @@ void print_hello(){
     //7
     printf("Day la code tu detached Head cua branch: user1_feature");
     print("Test upstream từ branch: user1_feature3");
+    //member1
+    //member2
 }
